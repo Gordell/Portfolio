e11789387e0ae29b1edc1c1d3c27b9247ad15c7f
Daniyal Usmanov <daniyal@MacBook-Pro.local>
2023-10-23 00:10:09 +0300

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,53 @@
+import Droov from '../assets/projects/droov.png'
+import Realtor from '../assets/projects/realtor.png'
+import Money from '../assets/projects/money.png'
+import Card from  '../assets/projects/card-game.png'
+import Dummflix from '../assets/projects/dummflix.png'
+import Weather from '../assets/projects/weather.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:Droov,
+        github:"",
+        live:"https://droov.io",
+    },
+    {
+        id:2,
+        name:"Realtor app",
+        image:Realtor,
+        github:"https://github.com/Gordell/realtor-clone-react.git",
+        live:"https://realtor-clone-react-810c1.web.app/",
+    },
+    {
+        id:3,
+        name:"Finance Tracking App",
+        image:Money,
+        github:"https://github.com/Gordell/Finance-Tracking.git",
+        live:"https://my-money-ca653.web.app/login",
+    },
+    {
+        id:4,
+        name:"Card Game",
+        image:Card,
+        github:"https://github.com/Gordell/Card-Game.git",
+        live:"https://card-game-2a25f.web.app",
+    },
+    {
+        id:5,
+        name:"Dummflix",
+        image:Dummflix,
+        github:"https://github.com/Gordell/Dummflix.git",
+        live:"https://dummflix-94bc7.firebaseapp.com",
+    },
+    {
+        id:6,
+        name:"Weather app",
+        image:Weather,
+        github:"https://github.com/Gordell/Weather.git",
+        live:"https://weather-ef7f2.web.app/",
+    },
+   
+
+]
